1a436e3322598cc3a9e7efef15b3b972746b69c7
babarali <babarsleekhive@gmail.com>
2025-12-12 12:11:00 -0800

    modify code

--- src/app/web-design/page.tsx
+++ src/app/web-design/page.tsx
@@ -1,22 +1,42 @@
-import { LandingHero } from "@/components/webdesignv2/LandingHero";
-import { LandingLogoSlider } from "@/components/webdesignv2/LandingLogoSlider";
-import { LandingAbout } from "@/components/webdesignv2/LandingAbout";
+import { Metadata } from "next";
+import { LandingHero } from "@/components/webdesign/LandingHero";
+import { WebDesignDevSuccess } from "@/components/webdesign/WebDesignDevSuccess";
+import { WebDevAI } from "@/sections/web-development/WebDevAI";
 import { NewPortfolio } from "@/sections/portfolio/NewPortfolio";
-import { CustomPlanCTA } from "@/components/webdesignv2/CustomPlanCTA";
-import { WebDesign2Testimonial } from "@/components/webdesignv2/WebDesign2Testimonial";
-import { LandingPricing } from "@/components/webdesignv2/LandingPricing";
+import { WebDesignCTA } from "@/components/webdesign/WebDesignCTA";
+import { TestimonialsSection } from "@/sections/reviews/TestimonialsSection";
+import { WebDesignVision } from "@/components/webdesign/WebDesignVision";
+import { WebDesignDevUX } from "@/components/webdesign/WebDevDesignUX";
+import { WebDesignLead } from "@/components/webdesign/WebDesignLead";
+import { WebDesignPopup } from "@/components/webdesign/WebDesignPopup";
+import { WebDesignFloatingCTA } from "@/components/webdesign/WebDesignFloatingCTA";
 
+import { PAGE_SEO, generateServiceSchema } from "@/lib/seo";
+import { generateMetadata as genMeta } from "@/lib/metadata";
 
-export default function WebDesignV2Page() {
+export const metadata: Metadata = genMeta(PAGE_SEO.webDevelopment);
+
+export default function WebDesignPage() {
+  const serviceSchema = generateServiceSchema();
+
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(serviceSchema) }}
+      />
+      <WebDesignPopup />
+      <WebDesignFloatingCTA />
       <LandingHero />
-      <LandingLogoSlider />
-      <LandingAbout />
-      <NewPortfolio />
-      <WebDesign2Testimonial />
-      <LandingPricing />
-      <CustomPlanCTA />
+      <WebDesignDevSuccess />
+      <WebDesignCTA />
+      <WebDevAI />
+      <NewPortfolio limit={9} />
+      <WebDesignCTA />
+      <TestimonialsSection />
+      <WebDesignVision />
+      <WebDesignDevUX />
+      <WebDesignLead />
     </>
   );
 }
