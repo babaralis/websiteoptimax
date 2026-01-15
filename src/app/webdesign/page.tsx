0096e5f3c2494910d387fdd3eb7e6c1f38105e58
babarali <babarsleekhive@gmail.com>
2026-01-15 12:38:38 -0800

    added script

--- src/app/webdesign/page.tsx
+++ src/app/webdesign/page.tsx
@@ -5,6 +5,7 @@ import { NewPortfolio } from "@/sections/portfolio/NewPortfolio";
 import { CustomPlanCTA } from "@/components/webdesignv2/CustomPlanCTA";
 import { WebDesign2Testimonial } from "@/components/webdesignv2/WebDesign2Testimonial";
 import { LandingPricing } from "@/components/webdesignv2/LandingPricing";
+import { PromoAmpWidget } from "@/components/webdesignv2/PromoAmpWidget";
 
 
 export default function WebDesignV2Page() {
@@ -17,6 +18,7 @@ export default function WebDesignV2Page() {
       <WebDesign2Testimonial />
       <LandingPricing />
       <CustomPlanCTA />
+      <PromoAmpWidget />
     </>
   );
 }
